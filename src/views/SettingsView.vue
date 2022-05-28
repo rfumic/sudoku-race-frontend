9ba61628533663dd2a8ba0f451a686080bd3374e
rfumic <rokofumic@gmail.com>
2022-05-28 20:01:19 +0200

    Added: changing email, username and password

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -11,7 +11,7 @@
         <div>
           username:
           <div class="right">
-            <p>userData</p>
+            <p>{{ username }}</p>
             <svg
               @click="update('username')"
               xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +33,7 @@
         <div>
           email:
           <div class="right">
-            <p>userData</p>
+            <p>{{ email }}</p>
             <svg
               @click="update('email')"
               xmlns="http://www.w3.org/2000/svg"
@@ -82,35 +82,46 @@
         <div>
           <label v-if="formInfo.labelText2">{{ formInfo.labelText2 }}</label>
           <input
-            type="text"
+            :type="formInfo.type2"
             :placeholder="formInfo.placeholder2"
             v-if="formInfo.placeholder2"
+            v-model="formInfo.input2"
           />
         </div>
         <label>{{ formInfo.labelText }}</label>
         <input
-          type="text"
+          :type="formInfo.type1"
           :placeholder="formInfo.placeholder"
           v-model="formInfo.input1"
         />
         <button @click="buttonFunction">update</button>
       </form>
     </div>
+    <transition name="fade">
+      <div v-if="error" class="error">
+        {{ error }}
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import { ref, computed, watch } from 'vue';
 import { useStore } from 'vuex';
-import { Service } from '@/services';
+import { Service, Auth } from '@/services';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const store = useStore();
+    const router = useRouter();
 
+    const username = ref(store.state.username);
+    const email = ref(store.state.userEmail);
     let showForm = ref(false);
     let formInfo = ref({});
     let buttonFunction = ref(null);
+    let error = ref('');
 
     function update(selected) {
       showForm.value = true;
@@ -138,28 +149,48 @@ export default {
 
     async function updateUsername() {
       formInfo.value = {
-        labelText: store.state.username,
+        labelText: username.value,
         labelText2: null,
         placeholder: 'new username',
         placeholder2: null,
         title: 'update username',
         input1: '',
         input2: '',
+        type1: 'text',
+        type2: 'text',
       };
-      buttonFunction.value = () => {
-        console.log('update username', formInfo.value.input1);
+      buttonFunction.value = async () => {
+        try {
+          await sendRequest({ new_username: formInfo.value.input1 });
+          Auth.logout();
+          router.go();
+        } catch (err) {
+          console.error(err);
+          error.value = 'error changing username';
+        }
       };
     }
     async function updateEmail() {
       formInfo.value = {
-        labelText: store.state.userEmail,
+        labelText: email.value,
         labelText2: null,
         placeholder: 'new email',
         placeholder2: null,
         title: 'update email',
+        input1: '',
+        input2: '',
+        type1: 'text',
+        type2: 'text',
       };
-      buttonFunction.value = () => {
-        console.log('update email');
+      buttonFunction.value = async () => {
+        try {
+          await sendRequest({ new_email: formInfo.value.input1 });
+          Auth.logout();
+          router.go();
+        } catch (err) {
+          console.error(err);
+          error.value = 'error changing email';
+        }
       };
     }
     async function updatePassword() {
@@ -169,10 +200,35 @@ export default {
         placeholder2: 'current password',
         labelText2: 'current password',
         title: 'update password',
+        input1: '',
+        input2: '',
+        type1: 'password',
+        type2: 'password',
       };
-      buttonFunction.value = () => {
-        console.log('update password');
+      buttonFunction.value = async () => {
+        if (formInfo.value.input1 != formInfo.value.input2) {
+          try {
+            const response = await sendRequest({
+              new_password: formInfo.value.input1,
+              old_password: formInfo.value.input2,
+            });
+            console.log(response);
+            router.replace('/');
+          } catch (err) {
+            console.error(err);
+            error.value = 'error changing password';
+          }
+        } else {
+          error.value = 'error changing password';
+        }
       };
+    }
+
+    async function sendRequest(data) {
+      const response = await Service.patch(`/users`, data);
+      if (!response) {
+        throw new Error();
+      }
     }
 
     return {
@@ -180,6 +236,9 @@ export default {
       update,
       formInfo,
       buttonFunction,
+      username,
+      email,
+      error,
     };
   },
 };
@@ -227,6 +286,7 @@ img {
 .info {
   display: flex;
   flex-direction: column;
+
   width: 90%;
   div {
     display: flex;
@@ -234,14 +294,19 @@ img {
   }
 
   p {
+    font-size: 1rem;
     color: $color-primary;
   }
   svg {
     width: 1.5rem;
+    padding-left: 1rem;
     &:hover {
       cursor: pointer;
       color: $color-primary;
     }
+  }
+  .right {
+    align-items: center;
   }
 }
 .password {
@@ -275,6 +340,7 @@ input {
   &::placeholder {
     color: $color-dark;
     opacity: 0.7;
+    font-size: 1rem;
   }
 }
 
@@ -303,6 +369,37 @@ form {
       box-shadow: 0 0 0 0 $color-white;
     }
   }
+}
+
+.error {
+  background: $color-warning;
+  padding: 0.5rem 1rem;
+  color: $color-white;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-enter-active {
+  transition: all 0.2s ease;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: all 0.2s ease;
 }
 
 @media (max-width: 770px) {
